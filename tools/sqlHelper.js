e341e5340465e79a7b6e65e4ac07ae8880ff6aef
chouzhenyi <zzyatbeijing@163.com>
2016-06-19 15:31:26 +0800

    聊天室

--- tools/sqlHelper.js
+++ tools/sqlHelper.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var client = mysql.createConnection({
     user: 'root',
-    password: ''
+    password: 'root'
 });
 client.connect();
 client.query('use zzyanode');
